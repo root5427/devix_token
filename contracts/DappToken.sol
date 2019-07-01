37a2c0e9063d0fb5facae09e2cbf5e5c37f09438
Devix Rootix <shukrik27@gmail.com>
2019-07-01 20:26:47 +0300

    1. Dev Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.8;
+
+contract DappToken {
+    // Constructer
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+    // Set the total number of tokens
+    // Read the total number of tokens
+}
